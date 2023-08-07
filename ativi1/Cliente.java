807226fa57d84fb4c2d65d4947e0cdede348ad3c
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2023-08-07 08:11:54 -0300

    Add files via upload

---
+++ ativi1/Cliente.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ativi1;
+
+/**
+ *
+ * @author alunolab08
+ */
+public class Cliente {
+    private String nome, cpf;
+
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public Cliente() {
+    }
+    
+}
---
+++ ativi1/Seguro.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ativi1;
+
+/**
+ *
+ * @author alunolab08
+ */
+public class Seguro {
+    private float valor;
+    private boolean vigencia;
+    private Cliente cliente;
+    private Veiculo veiculo;
+
+    public Seguro() {
+    }
+
+    public Seguro(float valor, boolean vigencia, Cliente cliente, Veiculo veiculo) {
+        this.valor = valor;
+        this.vigencia = vigencia;
+        this.cliente = cliente;
+        this.veiculo = veiculo;
+    }
+
+    
+    
+    
+}
